d26863df60426e5582c87239cf953b8b453395b6
Eric3023 <Mm5211314mM>
2020-05-19 16:25:53 +0800

    投放页面布局

--- pages/throw/throw.js
+++ pages/throw/throw.js
@@ -5,7 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    models: [
+      {
+        img: "/img/throw/model.png",
+        type: 0,
+      },
+      {
+        img: "/img/throw/model.png",
+        type: 0,
+      },
+      {
+        img: "/img/throw/model.png",
+        type: 0,
+      },
+      {
+        img: "/img/throw/model.png",
+        type: 0,
+      },
+    ],
+    mottos: [
+      "5万以下",
+      "5-10万",
+      "10-15万",
+      "15-20万",
+      "20-25万",
+      "25-30万",
+      "30-35万",
+      "35-50万",
+      "50-100万",
+      "100万以上",
+    ],
+    index: 0,
+    state: 0,//0：选择广告位，1：选择模板/文件；2.选择完成
   },
 
   /**
@@ -13,5 +44,41 @@ Page({
    */
   onLoad: function (options) {
 
+  },
+
+  /**
+   * 点击了上传素材
+   */
+  onClickUpdate(event) {
+    this.setData({
+      state: 1,
+    });
+  },
+
+  /**
+   * 取消了上传素材
+   */
+  onCancelMaterial() {
+    this.setData({
+      state: 0,
+    });
+  },
+
+  /**
+   * 上传素材
+   */
+  onCommitMaterial() {
+    this.setData({
+      state: 2,
+    });
+  },
+
+  /**
+   * 选中车型
+   */
+  bindPickerChange(event) {
+    this.setData({
+      index: event.detail.value,
+    })
   },
 })
